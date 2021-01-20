d151b78c8a9832c8e5ee5a46ab3df92adae8dc73
liaozhenyu_brian <brianliu1229@outlook.com>
2021-01-20 16:30:37 +0800

    implement history list view - on routing, add current view/route to history. - click on history item to navigate to it. - avoid duplicate entry. - cat (small label) is calculated by first node of routing path.

--- src/components/HistoryList.vue
+++ src/components/HistoryList.vue
@@ -17,25 +17,53 @@
 </template>
 
 <script>
-import { navigateToIfNeeded } from '../util/VueRouterHelper';
+import { navigateToIfNeeded, getExactPathForRouteWithParams } from '../util/VueRouterHelper';
 
 export default {
   name: 'HistoryList',
   data() {
     return {
       historyList: [
-        {viewName: "People List", cat: "People", path: "/people/list"},
-        {viewName: "People View", cat: "People", path: "/people/item/12/view"},
-        {viewName: "Message", cat: "People", path: "/message"},
+        // {viewName: "People List", cat: "People", path: "/people/list"},
+        // {viewName: "People View", cat: "People", path: "/people/item/12/view"},
+        // {viewName: "Message", cat: "People", path: "/message"},
       ],
     };
+  },
+  mounted(){
+    this.addCurrentRouteToHistory();
   },
   methods: {
     onCardClicked(historyItem){
       navigateToIfNeeded(this.$router, historyItem.path);
     },
+    addToHistoryList(route){
+      // if already exists, remove the existing before adding
+      let found = this.historyList.findIndex(it=>it.path === route.path);
+      if (found >= 0) {
+        this.historyList.splice(found, 1);
+      }
+      this.historyList.push(route);
+    },
+    addCurrentRouteToHistory(){
+      let firstMatchedRoute = this.$router.currentRoute.matched[0];
+      let lastMatchedRoute = 
+        this.$router.currentRoute.matched[this.$router.currentRoute.matched.length-1];
+      let currentRoutePath = getExactPathForRouteWithParams(
+        lastMatchedRoute.path, this.$router.currentRoute.params);
+      this.addToHistoryList({
+        viewName: lastMatchedRoute.name,
+        cat: firstMatchedRoute.name,
+        path: currentRoutePath,
+      });
+    }
   },
   components: {
+  },
+  watch: {
+    $route(to, from){
+      this.addCurrentRouteToHistory();
+    },
   },
 };
 </script>
