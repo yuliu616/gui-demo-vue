90752160208b0111d99b7ae7e29f52492df2d0d3
liaozhenyu_brian <brianliu1229@outlook.com>
2021-01-20 16:35:46 +0800

    add common message (translation)

--- src/translation/en/message.js
+++ src/translation/en/message.js
@@ -2,5 +2,9 @@ const message_text = {
   'sentence.login.passed': 'logged in successfully.',
   'sentence.login.failed': 'login failure.',
   'sentence.login.logoutDone': 'logged out.',
+  'sentence.creationSuccess': 'New record created (Record ID: {0}).',
+  'sentence.creationError': 'Server error when creating new record.',
+  'sentence.updateSuccess': 'Record updated successfully (version is now v{0}).',
+  'sentence.updateError': 'Server error when updating the record.',
 };
 export { message_text };
