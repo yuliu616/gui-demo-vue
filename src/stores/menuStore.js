4c1c80335bf1ee29a84d9cba11729ddc50f350eb
liaozhenyu_brian <brianliu1229@outlook.com>
2021-01-20 16:39:27 +0800

    remove dummy menu item

--- src/stores/menuStore.js
+++ src/stores/menuStore.js
@@ -2,7 +2,6 @@ const menuStore = {
   namespaced: true,
   state: {
     menuRoot: [
-      { code: 'Menu1', title: 'Menu1', targetPath: '/menu1' },
       { code: 'message', title: 'Messages', targetPath: '/message' },
     ],
   },
