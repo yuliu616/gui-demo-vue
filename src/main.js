2a0acc110570afa941509cb4f25bbd56b1b014c4
liaozhenyu_brian <brianliu1229@outlook.com>
2021-01-20 15:39:10 +0800

    add package vuex (store/state engine)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import Sidebar from './Sidebar.vue';
 import router from './router/router-index.vue';
+import { rootStore } from './stores/index';
 
 import 'semantic-ui-css/semantic.min.css';
 
@@ -10,11 +11,13 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   render: h => h(App),
+  store: rootStore,
 }).$mount('#app');
 
 new Vue({
   router,
   render: h => h(Sidebar),
+  store: rootStore,
 }).$mount('#sidebar');
 
 console.log(`vue app started using NODE_ENV=${process.env.NODE_ENV}`);
---
+++ src/stores/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+let rootStore = new Vuex.Store({
+  modules: {
+  }
+});
+
+// bootstrap
+// ...
+
+export { rootStore };
