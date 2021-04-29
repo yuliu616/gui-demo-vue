ff30083b1aec4ca35d36a3b20d9140f71ca364fd
liaozhenyu_brian <brianliu1229@outlook.com>
2021-04-29 18:36:40 +0800

    component LoadingPane - a panel to display a loading(spin) symbol.

---
+++ src/components/LoadingPane.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='my lead panel'>
+    <a-spin size="large" style="width:100%; margin: auto;" />
+    <div style="width:100%; text-align: center; margin-top: 3px;">
+      Loading ...
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'LoadingPane',
+});
+</script>
--- src/translation/en/message.ts
+++ src/translation/en/message.ts
@@ -6,5 +6,6 @@ const message_text = {
   'sentence.creationError': 'Server error when creating new record.',
   'sentence.updateSuccess': 'Record updated successfully (version is now v{0}).',
   'sentence.updateError': 'Server error when updating the record.',
+  'sentence.dataLoadingError': 'Failed to load data from server.',
 };
 export { message_text };
