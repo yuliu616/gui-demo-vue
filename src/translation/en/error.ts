9a9a84b4d81e0a3a7c80399996d00713408b5265
liaozhenyu_brian <brianliu1229@outlook.com>
2021-05-10 17:27:10 +0800

    i18n data for generic error 常用错误的国际化数据

--- src/translation/en/error.ts
+++ src/translation/en/error.ts
@@ -1,8 +1,17 @@
 const error = {
+  'ERROR_UNKNOWN': 'unknown error',
+  'INTERNAL_SERVER_ERROR': 'server error',
+  'ERROR_INVALID_USE_ERROR': 'invalid use',
   'ERROR_REQUIRED_FIELD': 'Field could not be empty',
   'ERROR_INVALID_FORMAT': 'Invalid format',
   'ERROR_INVALID_VALUE': 'Invalid value',
   'ERROR_MUST_BE_POSITIVE': 'Must be positive',
   'ERROR_AUTH_REFRESH': 'failed to refresh authentication',
+  'AXIOS_ERROR_NO_NETWORK': 'network unavailable',
+  'AXIOS_ERROR_ECONNABORTED': 'server response timed out',
+  'HTTP_ERROR_404': 'page not found',
+  'HTTP_ERROR_504': '504 network error',
+  'VALIDATION_ERROR': 'data rejected in validation',
+  'RECORD_NOT_FOUND': 'record not found',
 };
 export { error };
--- src/translation/zh/error.ts
+++ src/translation/zh/error.ts
@@ -1,8 +1,17 @@
 const error = {
+  'ERROR_UNKNOWN': '不明错误',
+  'INTERNAL_SERVER_ERROR': '后台错误',
+  'ERROR_INVALID_USE_ERROR': '违规作业',
   'ERROR_REQUIRED_FIELD': '不能留空',
   'ERROR_INVALID_FORMAT': '格式错误',
   'ERROR_INVALID_VALUE': '这是无效的值',
   'ERROR_MUST_BE_POSITIVE': '不可以是负数',
   'ERROR_AUTH_REFRESH': '登陆状态刷新失败',
+  'AXIOS_ERROR_NO_NETWORK': '没有网络',
+  'AXIOS_ERROR_ECONNABORTED': '后台反应超时',
+  'HTTP_ERROR_404': '页面不存在',
+  'HTTP_ERROR_504': '504 网络错误',
+  'VALIDATION_ERROR': '数据在校验中被拒',
+  'RECORD_NOT_FOUND': '数据不存在',
 };
 export { error };
