c32239d1603ccdcc837c853c31341e82f1366ee9
liaozhenyu_brian <brianliu1229@outlook.com>
2021-05-13 12:52:15 +0800

    Login view: add locale buttons and optimizing. - added buttons for switching locale. - minor UI/layout fix. - fix: background refreshing on logout.

--- src/Login.vue
+++ src/Login.vue
@@ -6,24 +6,34 @@
     :head-style="{
       'text-align': 'center',
     }"
-    title="GUI Demo">
+    :title="i18n.message['sentence.systemName']">
       <a-form layout="inline"
       :wrapper-col="{ span: 24 }" 
       @submit.prevent="submittingLoginForm()">
 
-        <a-form-item :label="i18n.model.LoginForm['field.username']">
+        <a-form-item :label="i18n.model.LoginForm['field.username']" class="w100">
           <a-input v-model="username"
           :placeholder="i18n.model.LoginForm['field.username']" auto-focus />
         </a-form-item>
-        <a-form-item :label="i18n.model.LoginForm['field.password']">
+        <a-form-item :label="i18n.model.LoginForm['field.password']" class="w100">
           <a-input-password v-model="password"
           :placeholder="i18n.model.LoginForm['field.password']" />
         </a-form-item>
 
-        <a-form-item style="margin-top: 1.4em;">
+        <a-form-item style="margin-top: 1.4em;" class="w100">
           <a-button type="primary" html-type="submit">
             {{ i18n.word['action.login'] }}
           </a-button>
+          <div class="my lead circular plain button sq40" 
+          v-on:click="changeLocale(LocaleCode.zh)"
+          style="font-size:85%; padding: 0; margin-left: 95px;">
+            中文
+          </div>
+          <div class="my milk circular plain button sq40" 
+          v-on:click="changeLocale(LocaleCode.en)"
+          style="font-size:85%; padding: 0;">
+            Eng
+          </div>
         </a-form-item>
 
       </a-form>
@@ -34,16 +44,16 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import { MessageType } from './stores/messageStore';
 import { MessageService } from '@/service/MessageService';
 import { AuthProvider } from '@/service/AuthProvider';
-import { i18n } from '@/translation/i18n';
+import VueRouter, { Route } from 'vue-router';
+import { LocaleCode } from './model/Locale';
+import { i18nModel, PreferenceStoreState } from './stores/preferenceStore';
 
 export default Vue.extend({
   name: 'Login',
   data(): ViewStateModel {
     return {
-      viewName: i18n.view['view.Login'],
       username: '',
       password: '',
       isMorningTime: false,
@@ -52,20 +62,30 @@ export default Vue.extend({
     };
   },
   computed: {
+    viewName(): string{ return this.i18n.view['view.Login']},
     iMessageService: ()=>MessageService(),
     iAuthProvider: ()=>AuthProvider(),
-    i18n: ()=>i18n,
+    iPreferenceStore(): PreferenceStoreState {
+      return this.$store.state.preferenceStore;
+    },
+    i18n(): i18nModel {
+      return this.iPreferenceStore.i18n;
+    },
     loggedIn(): boolean {
       return this.iAuthProvider.isLoggedIn();
     },
+    LocaleCode: ()=>LocaleCode,
   },
   mounted: function(){
-    let hourOfDay = new Date().getHours();
-    this.isMorningTime = (hourOfDay >= 6 && hourOfDay <= 11);
-    this.isDayTime = (hourOfDay > 11 && hourOfDay <= 19);
-    this.isNightTime = (hourOfDay > 19 || hourOfDay < 6);
+    this.refreshBackground();
   },
   methods: {
+    refreshBackground(){
+      let hourOfDay = new Date().getHours();
+      this.isMorningTime = (hourOfDay >= 6 && hourOfDay <= 11);
+      this.isDayTime = (hourOfDay > 11 && hourOfDay <= 19);
+      this.isNightTime = (hourOfDay > 19 || hourOfDay < 6);
+    },
     async submittingLoginForm(){
       try {
         await this.iAuthProvider.login({
@@ -76,7 +96,7 @@ export default Vue.extend({
       } catch(err){
         // for security reason, dont send error object to messageService
         await this.iMessageService.errorMsg(this, null,
-          i18n.message['sentence.login.failed'],
+          this.i18n.message['sentence.login.failed'],
         );
         return;
       }
@@ -84,13 +104,22 @@ export default Vue.extend({
       this.username = '';
       this.password = '';
     },
+    changeLocale(locale: LocaleCode){
+      this.$store.dispatch('preferenceStore/changeLocale', locale);
+    },
   },
   components: {
+  },
+  watch: {
+    loggedIn(newValue: boolean) {
+      if (!newValue) {
+        this.refreshBackground();
+      }
+    },
   },
 });
 
 interface ViewStateModel {
-  viewName: string;
   username: string;
   password: string;
   isMorningTime: boolean;
@@ -107,19 +136,19 @@ div.background {
 }
 
 div.background.morning {
-  background-image: url('./assets/morning.jpg');
+  background-image: url('/images/morning.jpg');
   background-position: -700px 0px;
   background-size: 220%;
 }
 
 div.background.dayTime {
-  background-image: url('./assets/afternoon.jpg');
+  background-image: url('/images/afternoon.jpg');
   background-position: 0px 0px;
   background-size: 200%;
 }
 
 div.background.night {
-  background-image: url('./assets/night.jpg');
+  background-image: url('/images/night.jpg');
   background-position: -240px 0px;
   background-size: 200%;
 }
@@ -175,8 +204,20 @@ div.background.night {
 }
 
 div.login.solo.card {
-  max-width: 24em;
-  margin: 25% auto;
+  max-width: 22em;
+  margin: auto;
+}
+@media screen and (min-height: 530px) {
+  /* tablet height */
+  div.login.solo.card {
+    margin: 10% auto;
+  }
+}
+@media screen and (min-height: 900px) {
+  /* desktop heigth */
+  div.login.solo.card {
+    margin: 30% auto;
+  }
 }
 
 div.login.header {
